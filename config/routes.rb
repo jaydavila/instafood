b4e991b7da7375709eb4843c2201196ef0f378b7
Jay Davila <jaydavila94@icloud.com>
2018-05-14 17:37:58 -0400

    build home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'pages/index'
+  get 'pages/show'
+  get 'pages/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'pages#index'
+  get 'pages/index', to: 'pages#index'
+
 end

3ebd76064f0ab369123d0bfea837e3f8acfc9849
Jay Davila <jaydavila94@icloud.com>
2018-05-17 20:57:18 -0400

    got update for recipes going

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -23,10 +23,25 @@ class RecipesController < ApplicationController
     end
   end
 
-  private
-
-  def recipe_params
-    params.require(:recipe).permit(:name, :description)
+  def edit
+    @recipe = Recipe.find(params[:id])
   end
 
-end
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(recipe_params)
+      flash[:success] = "Recipe was updated successfully!"
+      redirect_to recipe_path(@recipe)
+    else
+      render 'edit'
+    end
+  end
+
+
+  private
+
+    def recipe_params
+      params.require(:recipe).permit(:name, :description)
+    end
+
+  end

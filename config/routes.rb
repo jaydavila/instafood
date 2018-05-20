2ae287a535135c40e61266223ad6be0e95eab5c8
Jay Davila <jaydavila94@icloud.com>
2018-05-19 23:08:50 -0400

    made links/ badges for ingredients

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,16 @@
 Rails.application.routes.draw do
-  get 'pages/index'
-  get 'pages/show'
-  get 'pages/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#index'
   get 'pages/index', to: 'pages#index'
 
   resources :recipes
 
-    get '/signup', to: 'chefs#new'
+  get '/signup', to: 'chefs#new'
   resources :chefs, except: [:new]
 
-    get '/login', to: 'sessions#new'
+  get '/login', to: 'sessions#new'
   post '/login', to: "sessions#create"
   delete '/logout', to: "sessions#destroy"
+
+  resources :ingredients, except: [:destroy]
 end
